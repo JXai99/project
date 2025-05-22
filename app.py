b634b85672c31e695fc384bcc7f3b6e7e5bf8c09
“JX <j.cortezs@icloud.com.qgit config --global user.name JX Cgit config --global user.email j.cortezs@icloud.com>
2025-05-22 17:52:15 -0400

    Add helpers.py2

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+app.config["SECRET_KEY"] = "your_secret_key"
+#app.secret_key = "your_super_secret_key"
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///scores.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    return render_template("index.html")
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
