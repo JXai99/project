d67f4b24dd8643256bb371c00ae8c69898ca56e9
“JX <j.cortezs@icloud.com.qgit config --global user.name JX Cgit config --global user.email j.cortezs@icloud.com>
2025-05-24 13:11:53 -0400

    Register and login done

--- app.py
+++ app.py
@@ -42,6 +42,93 @@ def index():
     if request.method == "GET":
         print("homepage HOMEPAGE")
         return render_template("index.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    if request.method == "POST":
+        name = request.form.get("username")
+        if not name:
+            return apology("must provide username", 400)
+        pw1 = request.form.get("password")
+        pw2 = request.form.get("confirmation")
+        if not pw1 or not pw2:
+            return apology("must provide password", 400)
+        if pw1 != pw2:
+            flash("Passwords do not match!", "danger")
+            return apology("must provide password correctly", 400)
+
+        rows = db.execute("SELECT username FROM users")
+        for row in rows:
+
+            if (row["username"]) == name:
+                flash("Username already exist!", "warning")
+                return apology("Username already exist", 400)
+
+        print("username ok")
+        hash = generate_password_hash(pw1)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", name, hash)
+        rowsid = db.execute("SELECT id FROM users")
+        session["user_id"] = rowsid[0]["id"]
+        flash("Registered successfully!", "success")
+        # return render_template("quote.html")
+        return redirect("/login")
+
+    else:
+        return render_template("register.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
 
 #if __name__ == "__main__":
 #    app.run(debug=True)
